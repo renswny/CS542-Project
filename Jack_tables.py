0651c2deee78f0a1aaec7f9799918f46b71b3ada
Jack <tangjiesky@gmail.com>
2018-04-17 17:49:49 -0400

    Update Jack_tables.py

--- Jack_tables.py
+++ Jack_tables.py
@@ -6,13 +6,12 @@ class CreateTables(object):
     """create tables"""
     def __init__(self):
         self.engine = create_engine(
-            "mysql+pymysql://root:@localhost/Pokedex", "change your local path"
+            "mysql+pymysql://root:@localhost/Pokedex",
             echo=True)
         self.metadata = MetaData(self.engine)
         self.conn = self.engine.connect()
 
-   “”“In the table pokemon, we don't need HP, attack, speed, have attributes. I just delete these attributes"
-   def _pokemon(self):
+    def _pokemon(self):
         pokemon = Table(
             "pokemon", self.metadata,
             Column('pokedex', String(10), primary_key=true),
@@ -29,14 +28,26 @@ class CreateTables(object):
     def _HP(self):
         have_hp = Table(
             "have_hp", self.metadata,
-            Column('pokedex', String(10), primary_key=True, Foreignkey("pokemon.pokedex"), nullable=False)
             Column('hp', String(10), primary_key=True),
+            Column('pokedex', String(10), primary_key=True, ForeignKey("pokemon.pokedex"), nullable=False),
             Column('hp_percentile', Integer, nullable=False),
+        ) 
+        return have_hp
+
+    def _attack(self):
+        have_attack = Table(
+            "have_attack", self.metadata,
+            Column('attack', String(10), primary_key=True),
+            Column('pokedex', String(10), primary_key=True, ForeignKey("pokemon.pokedex"), nullable=False),
+            Column('att_percentile', Integer, nullable=False),
         )
-        return hp
+        return have_attack
+
+
     def run(self):
         pokemon = self._pokemon()
         have_hp = self._HP()
+        attack = self._attack()
         self.metadata.create_all(self.engine)
 
 if __name__ == '__main__':
